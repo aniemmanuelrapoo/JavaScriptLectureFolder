28d6475148194faf28804a3d32650e5ea7f7cb2f
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-05-05 20:53:19 +0100

    string methos

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 // console.error('This is a error')
 // console.warn('This is a warning')
 
-// variables
+//// variables
 //var let const  var is global scoped and not advised to be used. its old
 //let and const are es6
 //let you can reassign vaules
@@ -12,8 +12,8 @@
 // console.log(age);
 //const you cant reassign vaules
 
-//DataTypes
-//Strings, Numbers, Boolean, Null, Underfine
+////DataTypes
+////Strings, Numbers, Boolean, Null, Underfine
 // const name = "john";
 // const age = 30;
 // const rating = 4.5;
@@ -23,10 +23,21 @@
 // let z;
 // console.log(typeof z);
 
-// working with strings
-const name = 'john';
-const age = 30;
-//concatination
-console.log('my name is ' + name + ' and i am ' + age)
-//Template String
-console.log(`my name is ${name} and i am ${age}`)
+// // working with strings
+// const name = 'john';
+// const age = 30;
+// //concatination
+// console.log('my name is ' + name + ' and i am ' + age)
+// //Template String
+// console.log(`my name is ${name} and i am ${age}`)
+
+//more on strings methods
+const s = 'hello world'
+//number of letters
+console.log(s.length);
+console.log(s.toUpperCase);
+console.log(s.toLocaleLowerCase);
+//pulling out a particular text start and stop
+console.log(s.substring(0, 5).toUpperCase());
+//spliting a string to an array with anything you want to split by inside the box('')(',')
+console.log(s.split(''));
