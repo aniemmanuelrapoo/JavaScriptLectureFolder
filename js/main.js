89f116177e31eacb0a1a19ffccd70a1566402d58
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-05-05 20:36:17 +0100

    variables and DATAtypes

---
+++ js/main.js
@@ -0,0 +1,25 @@
+//console is a method of showing results to the browser
+// console.log('hello world')
+// console.error('This is a error')
+// console.warn('This is a warning')
+
+// variables
+//var let const  var is global scoped and not advised to be used. its old
+//let and const are es6
+//let you can reassign vaules
+// let age = 30;
+// age = 31
+// console.log(age);
+//const you cant reassign vaules
+
+//DataTypes
+//Strings, Numbers, Boolean, Null, Underfine
+const name = "john";
+const age = 30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z;
+
+console.log(typeof z);
