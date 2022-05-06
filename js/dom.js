02cb53c17dd893e32fd74185ff220f0ca86171b3
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-05-06 16:58:52 +0100

    manipulating the dom

--- js/dom.js
+++ js/dom.js
@@ -5,9 +5,20 @@
 //multipul element selector
 // console.log(document.querySelectorAll('.item'));
 
-//looping throught list item
-const items = document.querySelectorAll('.item')
+// //looping throught list item
+// const items = document.querySelectorAll('.item')
 
-items.forEach((item) => {
-  console.log(item);
-})
+// items.forEach((item) => {
+//   console.log(item);
+// })
+
+//Manipulating the DOM
+const ul = document.querySelector('.items');
+// ul.remove()
+// ul.lastElementChild.remove();
+ul.firstElementChild.textContent = 'hello'
+ul.children[1].innerText = 'rapoo'
+ul.lastElementChild.innerHTML = '<h4>Hello</h4>'
+
+const btn = document.querySelector('.btn')
+btn.style.background = 'red'
