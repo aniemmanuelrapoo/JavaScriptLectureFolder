aeb836d76443530583056bdbb4e79a38ccc952d4
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-05-06 17:15:45 +0100

    pratical time

--- js/dom.js
+++ js/dom.js
@@ -23,12 +23,22 @@
 // const btn = document.querySelector('.btn')
 // btn.style.background = 'red'
 
-//events
-const btn = document.querySelector('.btn')
+// //events
+// const btn = document.querySelector('.btn')
 
-btn.addEventListener('mouseout', (e) =>{
-  e.preventDefault()
-  document.querySelector('#my-form').style.background = '#ccc'
-  document.querySelector('body').classList.add('bg-dark');
-  document.querySelector('.items').lastElementChild.innerHTML = 'omo e dey'
-})
+// btn.addEventListener('mouseout', (e) =>{
+//   e.preventDefault()
+//   document.querySelector('#my-form').style.background = '#ccc'
+//   document.querySelector('body').classList.add('bg-dark');
+//   document.querySelector('.items').lastElementChild.innerHTML = 'omo e dey'
+// })
+
+
+//pratical
+const myForm = document.querySelector('#my-form')
+const nameInput = document.querySelector('#name')
+const emailInput = document.querySelector('#email')
+const msg = document.querySelector('.msg')
+const userList = document.querySelector('#users')
+
+my
