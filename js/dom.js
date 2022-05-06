2cbd6de3a16a7b90329230b6f7df2f5b995bbb20
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-05-06 17:11:45 +0100

    events

--- js/dom.js
+++ js/dom.js
@@ -12,13 +12,23 @@
 //   console.log(item);
 // })
 
-//Manipulating the DOM
-const ul = document.querySelector('.items');
-// ul.remove()
-// ul.lastElementChild.remove();
-ul.firstElementChild.textContent = 'hello'
-ul.children[1].innerText = 'rapoo'
-ul.lastElementChild.innerHTML = '<h4>Hello</h4>'
+// //Manipulating the DOM
+// const ul = document.querySelector('.items');
+// // ul.remove()
+// // ul.lastElementChild.remove();
+// ul.firstElementChild.textContent = 'hello'
+// ul.children[1].innerText = 'rapoo'
+// ul.lastElementChild.innerHTML = '<h4>Hello</h4>'
 
+// const btn = document.querySelector('.btn')
+// btn.style.background = 'red'
+
+//events
 const btn = document.querySelector('.btn')
-btn.style.background = 'red'
+
+btn.addEventListener('mouseout', (e) =>{
+  e.preventDefault()
+  document.querySelector('#my-form').style.background = '#ccc'
+  document.querySelector('body').classList.add('bg-dark');
+  document.querySelector('.items').lastElementChild.innerHTML = 'omo e dey'
+})
