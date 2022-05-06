0daa09f2153604397f144792bd6eb6f55b5693f7
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-05-06 13:35:55 +0100

    switch statment

--- js/main.js
+++ js/main.js
@@ -234,3 +234,15 @@ const x = 10;
 
 const color = x > 10 ? 'red' : 'blue';
 console.log(color);
+
+//switch statment
+switch(color){
+  case 'red':
+    console.log(`color is red`);
+    break;
+  case 'blue':
+    console.log('color is blue');
+    break
+  default:
+    console.log(`THERE IS NO COLOR`);
+}
