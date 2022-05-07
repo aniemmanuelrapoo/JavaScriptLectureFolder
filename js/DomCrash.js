23f2c3c8aa446671e0e8d24e30bd47025054110a
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-05-07 01:53:12 +0100

    next selectors

--- js/DomCrash.js
+++ js/DomCrash.js
@@ -1,17 +1,20 @@
-//Examine The DOCUMENT OBJECT
+// //Examine The DOCUMENT OBJECT
 
-console.dir(document); // to see all property attached to documnts
+// console.dir(document); // to see all property attached to documnts
 
-//examples
-console.log(document.domain);
-console.log(document.URL);
-console.log(document.title);
-// document.title = 123;
-console.log(document.doctype);
-console.log(document.haed);
-console.log(document.body);
-console.log(document.all);
-console.log(document.all[10]);
-// document.all[10].textContent = 'Hello';
-console.log(document.forms[0]);
-console.log(document.links);
+// //examples
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// // document.title = 123;
+// console.log(document.doctype);
+// console.log(document.haed);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// // document.all[10].textContent = 'Hello';
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+//Selectors
