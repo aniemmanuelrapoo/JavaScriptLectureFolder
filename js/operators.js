e0fcf25ba5fa6d8d295b60646cba9cf165f0673e
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-08-09 09:49:37 +0100

    Comparision Operators

--- js/operators.js
+++ js/operators.js
@@ -48,3 +48,30 @@ document.write(linebreak);
 
 
 //Comparision Operators
+var a = 10;
+var b = 20;
+var linebreak = "<br />";
+document.write("(a == b) => ");
+result = (a == b);
+document.write(result);
+document.write(linebreak);
+document.write("(a < b) => ");
+result = (a < b);
+document.write(result);
+document.write(linebreak);
+document.write("(a > b) => ");
+result = (a > b);
+document.write(result);
+document.write(linebreak);
+document.write("(a != b) => ");
+result = (a != b);
+document.write(result);
+document.write(linebreak);
+document.write("(a >= b) => ");
+result = (a >= b);
+document.write(result);
+document.write(linebreak);
+document.write("(a <= b) => ");
+result = (a <= b);
+document.write(result);
+document.write(linebreak);
