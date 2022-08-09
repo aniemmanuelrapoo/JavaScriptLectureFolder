b2d70a8057bf359cd2e0f05a860a75275423e415
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-08-09 09:47:56 +0100

    working on operators

---
+++ js/operators.js
@@ -0,0 +1,50 @@
+//Arithmetic Operators
+
+var a = 30;
+var b = 10;
+var c = "Test";
+var linebreak = "<br />";
+document.write("a + b = ");
+result = a + b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a - b = ");
+result = a - b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a * b = ");
+result = a * b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a / b = ");
+result = a / b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a % b = ");
+result = a % b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a + b + c = ");
+result = a + b + c;
+document.write(result);
+document.write(linebreak);
+
+a++;
+document.write("a++ = ");
+result = a;
+document.write(result);
+document.write(linebreak);
+
+b--;
+document.write("b-- = ");
+result = b;
+document.write(result);
+document.write(linebreak);
+
+
+//Comparision Operators
