18b923a57a85a121dfceb679d9697f5a9c6c1b1f
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-08-09 10:01:33 +0100

    Logical Operators

--- js/operators.js
+++ js/operators.js
@@ -81,3 +81,23 @@ document.write("(a <= b) => ");
 result = (a <= b);
 document.write(result);
 document.write(linebreak);
+
+//Logical Operators
+var a = true;
+var b = false;
+var linebreak = "<br />";
+
+document.write("(a && b) => ");
+result = (a && b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a || b) => ");
+result = (a || b);
+document.write(result);
+document.write(linebreak);
+
+document.write("!(a && b) => ");
+result = (!(a && b));
+document.write(result);
+document.write(linebreak);
