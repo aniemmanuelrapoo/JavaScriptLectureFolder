bbaca0bf9435ccd5f338a367ad4254e07c62081e
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-08-11 10:11:25 +0100

    typeof Operator

--- js/operators.js
+++ js/operators.js
@@ -177,3 +177,19 @@ document.write ("((a < b) ? 100 : 200) => ")
 result = (a < b) ? 100 : 200;
 document.write(result);
 document.write(linebreak);
+
+
+//typeof Operator
+var a = 10;
+var b = "String";
+var linebreak = "<br />";
+
+result = (typeof b == "string" ? "B is String" : "B is Numeric");
+document.write("Result => ");
+document.write(result);
+document.write(linebreak);
+
+result = (typeof a == "string" ? "A is String" : "A is Numeric");
+document.write("Result => ");
+document.write(result);
+document.write(linebreak);
