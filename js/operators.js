e8815fb7d07723eb2f5e0080010f7bddd82df651
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-08-11 09:32:14 +0100

    operators edited

--- js/operators.js
+++ js/operators.js
@@ -1,103 +1,121 @@
+//Operators
 //Arithmetic Operators
-
 var a = 30;
 var b = 10;
-var c = "Test";
-var linebreak = "<br />";
-document.write("a + b = ");
-result = a + b;
-document.write(result);
-document.write(linebreak);
+var c = "Test"
+var linebreak = "<br />"
 
-document.write("a - b = ");
-result = a - b;
-document.write(result);
-document.write(linebreak);
+//addition
+document.write("a + b = ")
+result = a + b
+document.write(result)
+document.write(linebreak)
 
-document.write("a * b = ");
-result = a * b;
-document.write(result);
-document.write(linebreak);
+//mins
+document.write("a - b = ")
+result = a - b
+document.write(result)
+document.write(linebreak)
 
-document.write("a / b = ");
-result = a / b;
-document.write(result);
-document.write(linebreak);
+//divsion
+document.write("a / b = ")
+result = a / b
+document.write(result)
+document.write(linebreak)
 
-document.write("a % b = ");
-result = a % b;
-document.write(result);
-document.write(linebreak);
+//multiplication
+document.write("a * b = ")
+result = a * b
+document.write(result)
+document.write(linebreak)
 
-document.write("a + b + c = ");
-result = a + b + c;
-document.write(result);
-document.write(linebreak);
+//Modulus
+document.write("a % b = ")
+result = a % b
+document.write(result)
+document.write(linebreak)
 
-a++;
-document.write("a++ = ");
-result = a;
-document.write(result);
-document.write(linebreak);
+//adding strings
+document.write("a + b + c = ")
+result = a + b + c
+document.write(result)
+document.write(linebreak)
 
-b--;
-document.write("b-- = ");
-result = b;
-document.write(result);
-document.write(linebreak);
+//Increment 
+a++
+document.write("a++ =")
+result = a
+document.write(result)
+document.write(linebreak)
+
+//Decrement
+b--
+document.write("b-- =")
+result = b
+document.write(result)
+document.write(linebreak)
 
 
 //Comparision Operators
-var a = 10;
-var b = 20;
-var linebreak = "<br />";
+var a = 20
+var b = 10
+var linebreak = "<br />"
 
-document.write("(a == b) => ");
-result = (a == b);
-document.write(result);
-document.write(linebreak);
+//equals
+document.write("(a === b) => ")
+result = (a === b)
+document.write(result)
+document.write(linebreak)
 
-document.write("(a < b) => ");
-result = (a < b);
-document.write(result);
-document.write(linebreak);
+//greater than
+document.write("(a > b) => ")
+result = (a > b)
+document.write(result)
+document.write(linebreak)
 
-document.write("(a > b) => ");
-result = (a > b);
-document.write(result);
-document.write(linebreak);
+//less than
+document.write("(a < b) => ")
+result = (a < b)
+document.write(result)
+document.write(linebreak)
 
-document.write("(a != b) => ");
-result = (a != b);
-document.write(result);
-document.write(linebreak);
+//greater than or equal
+document.write("(a >= b) => ")
+result = (a >= b)
+document.write(result)
+document.write(linebreak)
 
-document.write("(a >= b) => ");
-result = (a >= b);
-document.write(result);
-document.write(linebreak);
+//lessthan or equal
+document.write("(a <= b) => ")
+result = (a <= b)
+document.write(result)
+document.write(linebreak)
 
-document.write("(a <= b) => ");
-result = (a <= b);
-document.write(result);
-document.write(linebreak);
+//Not Equals to
+document.write("(a != b)")
+result = (a != b)
+document.write(result)
+document.write(linebreak)
+
 
 //Logical Operators
-var a = true;
-var b = false;
-var linebreak = "<br />";
+var a = true
+var b = true
 
-document.write("(a && b) => ");
-result = (a && b);
-document.write(result);
-document.write(linebreak);
+//Logical AND
+document.write("(a && b) => ")
+result = (a && b)
+document.write(result)
+document.write(linebreak)
 
-document.write("(a || b) => ");
-result = (a || b);
-document.write(result);
-document.write(linebreak);
+//Logical OR
+document.write("(a || b) => ")
+result = (a || b)
+document.write(result)
+document.write(linebreak)
 
-document.write("!(a && b) => ");
-result = (!(a && b));
-document.write(result);
-document.write(linebreak);
+//Logical NOT
+document.write("!(a && b) => ")
+result = (!(a && b))
+document.write(result)
+document.write(linebreak)
