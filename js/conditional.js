4fd2975ac8deca5175ba8ca7970499008910da80
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-08-15 08:46:01 +0100

    conditional if else elseif

---
+++ js/conditional.js
@@ -0,0 +1,46 @@
+// The syntax for a basic if statement is as follows:
+
+// if (expression){
+//   // Statement(s) to be executed if expression is true
+// }
+
+// var age = 20;
+// if( age > 18 ){
+//  document.write("<b>Qualifies for driving</b>");
+// }
+
+// //if and else syntax
+// if (expression){
+//   // Statement(s) to be executed if expression is true
+// }else{
+//   // Statement(s) to be executed if expression is false
+// }
+
+// var age = 15;
+// if( age > 18 ){
+//  document.write("<b>Qualifies for driving</b>");
+// }else{
+//  document.write("<b>Does not qualify for driving</b>");
+// }
+
+//if elseif and else syntax
+// if (expression 1){
+//   // Statement(s) to be executed if expression 1 is true
+//  }else if (expression 2){
+//   // Statement(s) to be executed if expression 2 is true
+//  }else if (expression 3){
+//   // Statement(s) to be executed if expression 3 is true
+//  }else{
+//   // Statement(s) to be executed if no expression is true
+//  }
+
+// var book = "maths";
+// if( book == "history" ){
+//  document.write("<b>History Book</b>");
+// }else if( book == "maths" ){
+//  document.write("<b>Maths Book</b>");
+// }else if( book == "economics" ){
+//  document.write("<b>Economics Book</b>");
+// }else{
+//  document.write("<b>Unknown Book</b>");
+// }
