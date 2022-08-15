9afe8ab68e06bee2a0293136fed7b4e2d6f2d950
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-08-15 09:03:18 +0100

    switch case

--- js/conditional.js
+++ js/conditional.js
@@ -44,3 +44,52 @@
 // }else{
 //  document.write("<b>Unknown Book</b>");
 // }
+
+
+//SWITCH CASE
+//SYNTAX of swtich case
+// switch (expression)
+// {
+//  case condition 1: statement(s)
+//  break;
+//  case condition 2: statement(s)
+//  break;
+//  ...
+//  case condition n: statement(s)
+//  break;
+//  default: statement(s)
+// }
+
+
+// var grade='A';
+// document.write("Entering switch block<br />");
+// switch (grade)
+// {
+//  case 'A': document.write("Good job<br />");
+//  break;
+//  case 'B': document.write("Pretty good<br />");
+//  break;
+//  case 'C': document.write("Passed<br />");
+//  break;
+//  case 'D': document.write("Not so good<br />");
+//  break;
+//  case 'F': document.write("Failed<br />");
+//  break;
+//  default: document.write("Unknown grade<br />")
+// }
+// document.write("Exiting switch block");
+
+
+//WITHOUT BREAK IT RUN ALL THE CODES
+// var grade='A';
+// document.write("Entering switch block<br />");
+// switch (grade)
+// {
+//  case 'A': document.write("Good job<br />");
+//  case 'B': document.write("Pretty good<br />");
+//  case 'C': document.write("Passed<br />");
+//  case 'D': document.write("Not so good<br />");
+//  case 'F': document.write("Failed<br />");
+//  default: document.write("Unknown grade<br />")
+// }
+// document.write("Exiting switch block");
