b523a2737b1fbe258f6a393d172ec5ded49ff6e5
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-08-16 02:12:43 +0100

    The do...whileLoop

--- js/loops.js
+++ js/loops.js
@@ -1,8 +1,17 @@
-//The while Loop
-var count = 5;
-document.write("Starting Loop<br /> ");
-while (count < 10){
+// //The while Loop
+// var count = 5;
+// document.write("Starting Loop<br /> ");
+// while (count < 10){
+//  document.write("Current Count : " + count + "<br />");
+//  count++;
+// }
+// document.write("Loop stopped!");
+
+//The do...whileLoop
+var count = 0;
+document.write("Starting Loop <br />");
+do{
  document.write("Current Count : " + count + "<br />");
  count++;
-}
-document.write("Loop stopped!");
+}while (count == 0);
+document.write ("Loop stopped!");
