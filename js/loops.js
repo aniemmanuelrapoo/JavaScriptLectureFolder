6eacf7d0ab0d5510edf6bf9a0a0017c36179c546
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-08-16 02:25:37 +0100

    The for Loop

--- js/loops.js
+++ js/loops.js
@@ -8,10 +8,18 @@
 // document.write("Loop stopped!");
 
 //The do...whileLoop
-var count = 0;
-document.write("Starting Loop <br />");
-do{
- document.write("Current Count : " + count + "<br />");
- count++;
-}while (count == 0);
-document.write ("Loop stopped!");
+// var count = 0;
+// document.write("Starting Loop <br />");
+// do{
+//  document.write("Current Count : " + count + "<br />");
+//  count++;
+// }while (count == 0);
+// document.write ("Loop stopped!");
+
+//The for Loop
+var count;
+document.write("Starting Loop<br />");
+for(count = 0; count < 10; count++){
+ document.write("Current Count : " + count + "<br />" );
+}
+document.write("Loop stopped!");
