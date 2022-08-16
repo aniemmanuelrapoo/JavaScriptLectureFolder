ab87883cab2709a5facda56a737ff826b42f987f
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-08-16 03:31:59 +0100

    rapping up loops

--- js/loops.js
+++ js/loops.js
@@ -85,18 +85,18 @@
 //  }
 //  document.write("Exiting the loop!<br /> ");
 
-//Using Labels to Control the Flow continue
-document.write("Entering the loop!<br /> ");
-outerloop: // This is the label name
-for (var i = 0; i < 3; i++)
-{
- document.write("Outerloop: " + i + "<br />");
- for (var j = 0; j < 5; j++)
- {
-  if (j == 3){
-  continue outerloop;
-  }
-  document.write("Innerloop: " + j + "<br />");
- } 
-}
-document.write("Exiting the loop!<br /> ");
+// //Using Labels to Control the Flow continue
+// document.write("Entering the loop!<br /> ");
+// outerloop: // This is the label name
+// for (var i = 0; i < 3; i++)
+// {
+//  document.write("Outerloop: " + i + "<br />");
+//  for (var j = 0; j < 5; j++)
+//  {
+//   if (j == 3){
+//   continue outerloop;
+//   }
+//   document.write("Innerloop: " + j + "<br />");
+//  } 
+// }
+// document.write("Exiting the loop!<br /> ");
