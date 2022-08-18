e8cec65a985b2b80bfb0f0325094cce42096f019
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-08-18 05:57:39 +0100

    Nested Functions

--- js/function.js
+++ js/function.js
@@ -16,15 +16,27 @@
 //  document.write (name + " is " + age + " years old.");
 // }
 
-//The return Statement => pass the secoundFunction to the html button
-function concatenate(first, last){
-  var full;
-  full = first + last;
-  return full;
- }
+// //The return Statement => pass the secoundFunction to the html button
+// function concatenate(first, last){
+//   var full;
+//   full = first + last;
+//   return full;
+// }
  
- function secondFunction(){
+// function secondFunction(){
+//   var result;
+//   result = concatenate('Zara', 'Ali');
+//   document.write (result );
+// }
+
+//Nested Functions => pass the secoundFunction to the html button
+function hypotenuse(a, b) {
+  function square(x) { return x*x; }
+  
+  return Math.sqrt(square(a) + square(b));
+}
+function secondFunction(){
   var result;
-  result = concatenate('Zara', 'Ali');
-  document.write (result );
- }
+  result = hypotenuse(2,2);
+  document.write ( result );
+}
