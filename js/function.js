11a564ad625114384512fe78120207dfc1d98244
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-08-18 05:29:33 +0100

    function Parameters

--- js/function.js
+++ js/function.js
@@ -4,8 +4,14 @@
 //  alert("Hello there");
 // }
 
-//calling a function => use a html button for onclick
-function sayHello() 
-{ 
- document.write ("Hello there!");
-} 
+// //calling a function => use a html button for onclick
+// function sayHello() 
+// { 
+//  document.write ("Hello there!");
+// } 
+
+//function Parameters => give the html buttton two parameter
+function sayHello(name, age)
+{
+ document.write (name + " is " + age + " years old.");
+}
